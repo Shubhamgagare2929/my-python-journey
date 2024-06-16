4f45551dd8ac74b120e91038b3238ed38dffa17c
shubhamgagare2929 <shubhamgagare63@gmail.com>
2024-06-16 22:46:27 +0530

    change of date 16 06 24

---
+++ decrementing-loop.py
@@ -0,0 +1,6 @@
+i=30
+while (i>0):
+    print(i)
+    i=i-1
+# this loop works in negative order
+# you can use it in decremrenating order also
--- while-loop.py
+++ while-loop.py
@@ -1,1 +1,13 @@
-print("i am back")
+i=int(input())
+while (i<=5):
+    i=int(input())
+    print("hey man")
+else:
+    print("i am in else")
+
+'''in  while loop statement get continuousy working
+until we stop it
+
+we can use else loop when the condition of while
+get completely finished
+or the while loop condition becomes false'''
